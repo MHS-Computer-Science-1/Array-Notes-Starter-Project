755c51c08a253a96e013ad306d4507d9952364fb
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2024-10-11 10:48:41 -0400

    Add files via upload

---
+++ Arrays Notes/Arrays Notes/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        PictureBox1 = New PictureBox()
+        Button4 = New Button()
+        ListBox1 = New ListBox()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.Black
+        Button1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.ForeColor = Color.White
+        Button1.Location = New Point(14, 14)
+        Button1.Margin = New Padding(4, 3, 4, 3)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(177, 27)
+        Button1.TabIndex = 0
+        Button1.Text = "What is an Array?"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.Black
+        Button2.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Button2.ForeColor = Color.White
+        Button2.Location = New Point(14, 182)
+        Button2.Margin = New Padding(4, 3, 4, 3)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(177, 27)
+        Button2.TabIndex = 2
+        Button2.Text = "2D Arrays"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.Black
+        Button3.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Button3.ForeColor = Color.White
+        Button3.Location = New Point(14, 148)
+        Button3.Margin = New Padding(4, 3, 4, 3)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(177, 27)
+        Button3.TabIndex = 4
+        Button3.Text = "Bubble Sort"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(676, 49)
+        PictureBox1.Margin = New Padding(4, 3, 4, 3)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(348, 126)
+        PictureBox1.TabIndex = 5
+        PictureBox1.TabStop = False
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(679, 180)
+        Button4.Margin = New Padding(4, 3, 4, 3)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(88, 27)
+        Button4.TabIndex = 6
+        Button4.Text = "Restart"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.BackColor = Color.Black
+        ListBox1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        ListBox1.ForeColor = Color.White
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 21
+        ListBox1.Location = New Point(212, 14)
+        ListBox1.Margin = New Padding(4, 3, 4, 3)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(430, 214)
+        ListBox1.TabIndex = 7
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = Color.Black
+        Button5.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Button5.ForeColor = Color.White
+        Button5.Location = New Point(14, 47)
+        Button5.Margin = New Padding(4, 3, 4, 3)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(177, 27)
+        Button5.TabIndex = 8
+        Button5.Text = "Access Array Elements"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = Color.Black
+        Button6.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Button6.ForeColor = Color.White
+        Button6.Location = New Point(14, 81)
+        Button6.Margin = New Padding(4, 3, 4, 3)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(177, 27)
+        Button6.TabIndex = 9
+        Button6.Text = "Looping through the Array"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = Color.Black
+        Button7.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        Button7.ForeColor = Color.White
+        Button7.Location = New Point(13, 114)
+        Button7.Margin = New Padding(4, 3, 4, 3)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(177, 27)
+        Button7.TabIndex = 10
+        Button7.Text = "Linear Search"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.DarkTurquoise
+        ClientSize = New Size(655, 263)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(ListBox1)
+        Controls.Add(Button4)
+        Controls.Add(PictureBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Margin = New Padding(4, 3, 4, 3)
+        Name = "Form1"
+        Text = "Array Notes"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+End Class
---
+++ Arrays Notes/Arrays Notes/Form1.vb
@@ -0,0 +1,199 @@
+﻿Public Class Form1
+    'ARRAY NOTES
+
+    'Learning Goals
+    '---------------
+    'I can create and use arrays to solve problems in Visual BASIC
+    'I can use loops to process all the elements of an array
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        'What is an array?
+        ListBox1.Items.Add("An ARRAY is a variable that can hold a list of information")
+
+        ListBox1.Items.Add("Examples:")
+        ListBox1.Items.Add(" - Grocery List")
+        ListBox1.Items.Add(" - Your grades in a class")
+        ListBox1.Items.Add(" - A list of the states and capitals for a trivia game")
+
+        'How do I make an array that holds 5 elements?
+
+
+        'testScores = [0, 0, 0, 0, 0]
+        '      index:  0  1  2  3  4
+
+        'The INDEX number identifies the location in the array
+        'We can use this index number to access/change array elements
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        'Accessing Array Elements
+        Dim testScores(4) As Integer
+        'testScores = [95, 78, 0, 0, 0]
+        '      index:  0    1  2  3  4
+
+        'Assign elements
+
+
+        'Try it: assign the 2nd score to 78
+
+
+        'Try it: assign the last score to a number entered
+        'in an input box
+
+
+
+        'Display all the scores in the Listbox (Long Way)
+        ListBox1.Items.Add("Test Scores")
+
+
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        'Looping through an array
+        Dim testScores(29) As Integer
+
+        Dim r As New Random
+
+        testScores(0) = r.Next(75, 101)
+        testScores(1) = r.Next(75, 101)
+        testScores(2) = r.Next(75, 101)
+        testScores(3) = r.Next(75, 101)
+        testScores(4) = r.Next(75, 101)
+        testScores(5) = r.Next(75, 101)
+        testScores(6) = r.Next(75, 101)
+        testScores(7) = r.Next(75, 101)
+        testScores(8) = r.Next(75, 101)
+        testScores(9) = r.Next(75, 101)
+        testScores(10) = r.Next(75, 101)
+        testScores(11) = r.Next(75, 101)
+        testScores(12) = r.Next(75, 101)
+        testScores(13) = r.Next(75, 101)
+        testScores(14) = r.Next(75, 101)
+        testScores(15) = r.Next(75, 101)
+        testScores(16) = r.Next(75, 101)
+        testScores(17) = r.Next(75, 101)
+        testScores(18) = r.Next(75, 101)
+        testScores(19) = r.Next(75, 101)
+        testScores(20) = r.Next(75, 101)
+        testScores(21) = r.Next(75, 101)
+        testScores(22) = r.Next(75, 101)
+        testScores(23) = r.Next(75, 101)
+        testScores(24) = r.Next(75, 101)
+        testScores(25) = r.Next(75, 101)
+        testScores(26) = r.Next(75, 101)
+        testScores(27) = r.Next(75, 101)
+        testScores(28) = r.Next(75, 101)
+        testScores(29) = r.Next(75, 101)
+
+        'Display all scores with a loop
+        ListBox1.Items.Add("All Scores")
+
+
+
+        'Display As
+        ListBox1.Items.Add("The As")
+
+
+
+        'Try it: Display the Bs
+        ListBox1.Items.Add("The Bs")
+
+
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        'Linear Search
+        Dim numbers(99) As Integer
+
+        'Fill the array with random numbers
+        Dim r As New Random
+        Dim i As Integer
+        For i = 0 To 99
+            numbers(i) = r.Next(0, 125)
+        Next
+
+        'Define what we are searching for
+
+
+        'How can we know if toFind wasn't found?
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim y, x, temp As Integer
+
+        Dim numbers(7) As Integer
+        numbers(0) = 6
+        numbers(1) = 5
+        numbers(2) = 3
+        numbers(3) = 1
+        numbers(4) = 8
+        numbers(5) = 7
+        numbers(6) = 2
+        numbers(7) = -4
+
+        'Bubble Sort
+        '************
+        For y = 0 To numbers.Length - 2 'Repeat this process over and over
+            For x = 0 To numbers.Length - 2 'Loop through the elements
+                'Is element x bigger than element x+1?
+                If numbers(x) > numbers(x + 1) Then
+                    'Flip-flop element x and x+1
+                    temp = numbers(x)
+                    numbers(x) = numbers(x + 1)
+                    numbers(x + 1) = temp
+                End If
+            Next
+        Next
+
+
+        ListBox1.Items.Add("After Bubble Sort:")
+        For i = 0 To numbers.Length - 1
+            ListBox1.Items.Add(numbers(i))
+        Next
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        'Create a 2-d array
+        Dim myArray(2, 3) As Integer '3 rows, 4 cols
+        'myArray:  0 0 0 0
+        '          0 0 0 0
+
+        'Access the elements using a row # and col #
+
+
+        'myArray:  5 0 0 0
+        '          0 0 0 1
+
+        'Try it: Set the top-right corner and bottom left
+        'corner to 3 and 7 respectively
+
+
+
+        'Set all the elements to 10 (The loop way)
+        Dim row, col As Integer
+
+
+
+
+        'myArray:  10 10 10 10
+        '          10 10 10 10
+
+    End Sub
+
+
+
+
+
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        PictureBox1.Image = My.Resources.Bubble_sort_example_300px
+    End Sub
+
+
+End Class
